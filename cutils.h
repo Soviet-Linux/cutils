00b3f13a35d5feb6c6e55a81b22289a2d9e6488e
pkd <paulkronlund@gmail.com>
2024-05-02 20:04:26 +0000

    I hope this works

--- cutils.h
+++ cutils.h
@@ -48,8 +48,9 @@ int wrnfile(const char* filePath,char* buffer,long size);
         * 2 - Not a directory
 */
 int xisdir (const char *d);
+#define isdir(d) xisdir(d)
 // for some reason some versions of libspm use isdir instead of xisdir
-#define isdir xisdir
+
 // create dir recursivelty (similar to mkdir -p)
 int pmkdir (const char *dir);
 //  move a file and create the dir if it doesn't exist
